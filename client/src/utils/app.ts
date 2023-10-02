b50de6ec157b50170f7671937db9298d3767aafd
CJ <cjfritz9@gmail.com>
2023-10-02 10:14:17 -0600

    fixed a typo

--- client/src/utils/app.ts
+++ client/src/utils/app.ts
@@ -1,7 +1,7 @@
 const validateColorMode = () => {
   const chakraColorKey = 'chakra-ui-color-mode';
   const colorMode = localStorage.getItem(chakraColorKey);
-  if (colorMode === 'lighht') {
+  if (colorMode !== 'dark') {
     localStorage.setItem(chakraColorKey, 'dark');
   }
 };
